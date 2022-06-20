ce861df9363f40bf56b8803e6158153cbb54697f
AhnSeung <corokuru@nate.com>
2022-06-20 14:26:08 +0900

    Initial commit

---
+++ src/main/java/com/nice/springBoot/nicerestfulservice/NiceRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nice.springBoot.nicerestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceRestfulServiceApplication.class, args);
+	}
+
+}
