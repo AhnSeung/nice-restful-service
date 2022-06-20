e22c45ea0138272ad81e4322f3b5004d1cc2d996
AhnSeung <corokuru@nate.com>
2022-06-20 14:30:50 +0900

    basic controller add

---
+++ src/main/java/com/nice/springBoot/nicerestfulservice/controller/NiceRestfulController.java
@@ -0,0 +1,8 @@
+package com.nice.springBoot.nicerestfulservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NiceRestfulController {
+
+}
